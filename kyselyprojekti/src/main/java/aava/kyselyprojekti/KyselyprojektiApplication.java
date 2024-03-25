ca559767ea2964b86eaf5f68582e0b35b2b4f403
Akkel-i <118986817+Akkel-i@users.noreply.github.com>
2024-03-25 10:29:57 +0200

    created maven project

---
+++ kyselyprojekti/src/main/java/aava/kyselyprojekti/KyselyprojektiApplication.java
@@ -0,0 +1,13 @@
+package aava.kyselyprojekti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyselyprojektiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyselyprojektiApplication.class, args);
+	}
+
+}
---
+++ kyselyprojekti/src/test/java/aava/kyselyprojekti/KyselyprojektiApplicationTests.java
@@ -0,0 +1,13 @@
+package aava.kyselyprojekti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyselyprojektiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

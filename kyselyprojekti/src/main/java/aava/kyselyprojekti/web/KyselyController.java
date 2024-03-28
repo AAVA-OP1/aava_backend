f2752998b7cb4524b9ef1128cff5597702ad9a2b
AlinaLokkinen <bhd524@myy.haaga-helia.fi>
2024-03-28 12:47:44 +0200

    headerit lisätty sivuille ja eka nappi tehty

---
+++ kyselyprojekti/src/main/java/aava/kyselyprojekti/web/KyselyController.java
@@ -0,0 +1,21 @@
+package aava.kyselyprojekti.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class KyselyController {
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String aloitusSivu() {
+        return "index"; //.html
+    }
+
+    @RequestMapping(value = "/uusikysely", method = RequestMethod.GET)
+    public String uusiKysely() {
+        return "uusikysely"; //.html
+    }
+    
+}

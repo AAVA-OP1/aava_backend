295a43308cde39700e48ee8e9a5b43113b2148ca
AlinaLokkinen <bhd524@myy.haaga-helia.fi>
2024-03-28 13:01:47 +0200

    Repositoryt luotu

---
+++ kyselyprojekti/src/main/java/aava/kyselyprojekti/domain/KysymysRepository.java
@@ -0,0 +1,9 @@
+package aava.kyselyprojekti.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface KysymysRepository extends CrudRepository<Kysymys, Long>{
+
+
+
+}

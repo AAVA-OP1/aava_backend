d25be4051992bb03bb95bb70a73ee67c9f2c59eb
AlinaLokkinen <bhd524@myy.haaga-helia.fi>
2024-04-02 10:34:07 +0300

    nappeja lisätty ja jotain kontrollerimetodeita

--- kyselyprojekti/src/main/java/aava/kyselyprojekti/web/KyselyController.java
+++ kyselyprojekti/src/main/java/aava/kyselyprojekti/web/KyselyController.java
@@ -2,10 +2,13 @@ package aava.kyselyprojekti.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import aava.kyselyprojekti.domain.Kysely;
 import aava.kyselyprojekti.domain.KyselyRepository;
+import aava.kyselyprojekti.domain.Kysymys;
 import aava.kyselyprojekti.domain.KysymysRepository;
 
 
@@ -21,14 +24,35 @@ public class KyselyController {
 
     // aloitussivun endpoint
     @RequestMapping(value = "/index", method = RequestMethod.GET)
-    public String aloitusSivu() {
+    public String aloitusSivu(Model model) {
+
+        model.addAttribute("kyselyt", kyselyRepository.findAll());
         return "index"; //.html
     }
 
     // uuden kyselyn endpoint
     @RequestMapping(value = "/uusikysely", method = RequestMethod.GET)
-    public String uusiKysely() {
+    public String uusiKysely(Model model) {
+
+        model.addAttribute("kysely", new Kysely());
+
         return "uusikysely"; //.html
     }
+
+    @RequestMapping(value = "/uusikysymys", method = RequestMethod.GET)
+    public String uusiKysymys(Model model) {
+
+        model.addAttribute("kysymys", new Kysymys());
+
+        return "uusikysymys"; // .html
+    }
+
+    // aloitettu avaakysely -metodi:
+    // @RequestMapping(value = "/katsokysely/{id}", method = RequestMethod.GET)
+    // public String katsoKysely() {
+
+    //     return "katsokysely"; //.html
+    // }
+
     
 }

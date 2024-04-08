5aa4df01b31dd06a7d0b523f7968c971532e7872
AlinaLokkinen <bhd524@myy.haaga-helia.fi>
2024-04-08 09:10:12 +0300

    Controllerit jaettu omiinsa

--- kyselyprojekti/src/main/java/aava/kyselyprojekti/web/KyselyController.java
+++ kyselyprojekti/src/main/java/aava/kyselyprojekti/web/KyselyController.java
@@ -1,6 +1,5 @@
 package aava.kyselyprojekti.web;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -73,52 +72,8 @@ public class KyselyController {
             return "tarkastelekyselya"; // .html
         } else
             return "redirect:/index";
-    }
-
-    // en ymmärrä, help
-    @RequestMapping(value = "/uusikysymys", method = RequestMethod.GET)
-    public String uusiKysymys(Model model) {
-
-        // Kysymys -luokassa tyhjän kysymyksen konstruktori laittaa id:ksi aina 0
-        model.addAttribute("kysymys", new Kysymys());
-
-        List<Kysely> kyselyt = (List<Kysely>) kyselyRepository.findAll();
-        model.addAttribute("kyselyt", kyselyt);
-
-        List<Kysymys> kysymykset = (List<Kysymys>) kysymysRepository.findAll();
-        model.addAttribute("kysymykset", kysymykset);
-
-        /*
-         * Long size = (long) kyselyt.size();
-         * Optional<Kysely> kysely = kyselyRepository.findById(size);
-         * model.addAttribute("linkitettyKysely", kysely);
-         */
-
-        Long size = (long) kyselyt.size();
-        Optional<Kysely> kysely = kyselyRepository.findById(size);
-        if (kysely.isPresent()) {
-            Kysely kysely1 = kysely.get();
-            model.addAttribute("linkitettyKysely", kysely1);
-        }
-
-        return "uusikysymys"; // .html
-    }
+    } 
 
     
-    @RequestMapping(value = "/tallennakysymys", method = RequestMethod.POST)
-    public String tallennaKysymys(Kysymys uusiKysymys, Model model) {
-
-        kysymysRepository.save(uusiKysymys);
-
-        return "redirect:/uusikysymys";
-    }
-
-    // tämä turha? koska tarkastelekyselya() 
-    // aloitettu avaakysely -metodi:
-    // @RequestMapping(value = "/katsokysely/{id}", method = RequestMethod.GET)
-    // public String katsoKysely() {
-
-    // return "katsokysely"; //.html
-    // }
-
+    
 }
---
+++ kyselyprojekti/src/main/java/aava/kyselyprojekti/web/KysymysController.java
@@ -0,0 +1,57 @@
+package aava.kyselyprojekti.web;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import aava.kyselyprojekti.domain.Kysely;
+import aava.kyselyprojekti.domain.KyselyRepository;
+import aava.kyselyprojekti.domain.Kysymys;
+import aava.kyselyprojekti.domain.KysymysRepository;
+
+@Controller
+public class KysymysController {
+    
+    @Autowired
+    private KysymysRepository kysymysRepository;
+
+    @Autowired
+    private KyselyRepository kyselyRepository;
+
+      @RequestMapping(value = "/uusikysymys", method = RequestMethod.GET)
+    public String uusiKysymys(Model model) {
+
+        // Kysymys -luokassa tyhjän kysymyksen konstruktori laittaa id:ksi aina 0
+        model.addAttribute("kysymys", new Kysymys());
+
+        List<Kysely> kyselyt = (List<Kysely>) kyselyRepository.findAll();
+        model.addAttribute("kyselyt", kyselyt);
+
+        List<Kysymys> kysymykset = (List<Kysymys>) kysymysRepository.findAll();
+        model.addAttribute("kysymykset", kysymykset);
+
+
+        Long size = (long) kyselyt.size();
+        Optional<Kysely> kysely = kyselyRepository.findById(size);
+        if (kysely.isPresent()) {
+            Kysely kysely1 = kysely.get();
+            model.addAttribute("linkitettyKysely", kysely1);
+        }
+
+        return "uusikysymys"; // .html
+    }
+
+    @RequestMapping(value = "/tallennakysymys", method = RequestMethod.POST)
+    public String tallennaKysymys(Kysymys uusiKysymys, Model model) {
+
+        kysymysRepository.save(uusiKysymys);
+
+        return "redirect:/uusikysymys";
+    }
+
+}
